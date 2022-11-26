90c91e7f6d17e16d70e5ab93effff112b5b96ac3
Lucas Dantas <lucasdesousadantas@gmail.com>
2022-11-26 09:41:30 -0300

    backend finish

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const todosRoutes = require('./todos.routes.js')
 app.use(express.json())
 app.use(todosRoutes)
 app.get('/', (req, res) => {
-    return res.send("oei")
+    return res.send("Funcionando")
 })
 app.listen('3000', () => {
     console.log('rodando')
