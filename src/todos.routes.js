f5cad56ad7ad74121d99c69842d6a63c1b4eed38
Lucas Dantas <lucasdesousadantas@gmail.com>
2022-11-26 08:21:29 -0300

    docker configuration

--- src/todos.routes.js
+++ src/todos.routes.js
@@ -8,8 +8,11 @@ todosRoutes.post('/todos', (req, res) => {
     allTodos.push({name, status})
     return res.status(201).json(allTodos)
 
-
 })
 
+todosRoutes.get('/todos', (req, res) => {
+    const resposta = allTodos
+    return res.status(200).json(allTodos)
+})
 
 module.exports = todosRoutes
